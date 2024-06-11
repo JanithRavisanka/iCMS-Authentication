dbce61ce78b09ffedb23f54007b771a3d4a7c238
Wilbax <janithravisankax@gmail.com>
2024-06-11 23:02:42 +0530

    added #major update on workflow

--- app/routers/userAdmin.py
+++ app/routers/userAdmin.py
@@ -17,8 +17,8 @@ aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
 aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
 aws_default_region = os.getenv('AWS_DEFAULT_REGION')
 
-print(aws_access_key_id)
-print(aws_secret_access_key)
+# print(aws_access_key_id)
+# print(aws_secret_access_key)
 
 admin_router = APIRouter()
 cognito_client = client(
